db7c04b6596fb6c9252010bc98f59b9f3724a1d4
Ledezma <eledezm@amazon.com>
2023-01-18 19:01:31 -0700

    lines

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -13,7 +13,7 @@ console.log("working");
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  id: 'mapbox/light-v10',
+  id: 'mapbox/dark-v10',
   tileSize: 512,
   zoomOffset: -1,
   accessToken: API_KEY
@@ -46,17 +46,28 @@ L.control.layers(baseMaps).addTo(map);
 light.addTo(map);
 
 // Accessing the airport GeoJSON URL
-// let airportData = "https://raw.githubusercontent.com/EricaLedezma/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/EricaLedezma/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/EricaLedezma/Mapping_Earthquakes/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/EricaLedezma/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+    }
+  }).addTo(map);
 });
   
 
